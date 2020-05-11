a8a56f16c8e21edb19b84d4c8262d96a85f59ffb
Franklin-Molina <asdfglxl7@gmail.com>
2020-05-11 18:46:05 -0500

    error 404

---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:p_crud/ui/listview_product.dart';
+
+void main() => runApp(new MaterialApp(
+  home: ListViewProduct(),
+  title: 'Product Db',
+));
+
